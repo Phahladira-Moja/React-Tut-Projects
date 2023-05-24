ea803235d11333f8d826b76bb95ef2cb38cb6935
Phahladira-Moja <phahladira@patala.co.za>
2023-05-24 23:33:34 +0200

    Added the geberate Unique Id for chat messages, as well as chatStripe generator function to distinguish between chats.

--- open_ai_codex/script.js
+++ open_ai_codex/script.js
@@ -30,3 +30,38 @@ function typeText(element, text) {
     }
   }, 20);
 }
+
+function generateUniqueId() {
+  const timestamp = Date.now;
+  const randomNumber = Math.random();
+  const hexadecimal = randomNumber.toString();
+
+  return `id=${timestamp}-${hexadecimal}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+      <div class="wrapper" ${isAi && "ai"}>
+        <div class="chat">
+          <div className="profile">
+            <img 
+              src="${isAi ? bot : user}"
+              alt="${isAi ? "bot" : "user"}"
+            />
+          </div>
+          <div class="message" id=${uniqueId}>
+            ${value}
+          </div>
+        </div>
+      </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+};
